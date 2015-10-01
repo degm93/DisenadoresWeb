2cdf27fc3801fbbba1c6f465df5164b108783c6b
Diego <de.gm93@gmail.com>
2015-10-01 09:27:25 -0600

    Carga inicial Disenadores Web

---
+++ DisenadoresWeb/DisenadoresWeb/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisenadoresWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
